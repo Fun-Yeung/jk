09687068f1b4815355b5e966950dc79b406f508c
Fun-Yeung <51143674+Fun-Yeung@users.noreply.github.com>
2019-06-10 18:31:46 +0800

    Add files via upload

---
+++ 未命名1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int main(){
+ 	printf("ÇëÊäÈë1»ò2\n");
+ 	int a;
+ 	scanf("%d",&a);
+ 	if(a==1){
+ 		printf("ÄãºÃ°ôÅ¶");
+	 }else if(a==2){
+	 	printf("ÄãÕæÀ÷º¦");
+	 }else{
+	 	printf("À±¼¦");
+	 }
+	 return 0;
+ }
